3159e6e016bbafc6ec5e36f92dc52e73ae0a9b29
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2024-10-29 02:51:05 +0600

    send dammy data get method

--- Index.js
+++ Index.js
@@ -2,28 +2,40 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app
-  .get("/", (req, res) => {
-    res.end("hello express");
-  })
-  .get("/about", (req, res) => {
-    res.end("this is the about page");
-  })
-  .get("/contact", (req, res) => {
-    res.end("this is the contact page");
-  })
-  .get("/service", (req, res) => {
-    res.end("this is the service page");
-  })
-  .get("/team", (req, res) => {
-    res.end("this is the team page");
-  })
-  .put("/", (req, res) => {
-    res.end("hello express, this is the home page with PUT");
-  })
-  .delete("/", (req, res) => {
-    res.end("this is the delete action on home page");
-  })
-  .listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
-  });
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+app.get("/", (req, res) => {
+  res.send("this is home route ");
+});
+app.post("/", (req, res) => {
+  res.send(req.body);
+});
+app.get("/user", (req, res) => {
+  let data = [
+    {
+      name: "mahtab",
+      email: "withgaming50@gmail.com",
+    },
+    {
+      name: "mahtab",
+      email: "withgaming50@gmail.com",
+    },
+    {
+      name: "mahtab",
+      email: "withgaming50@gmail.com",
+    },
+    {
+      name: "mahtab",
+      email: "withgaming50@gmail.com",
+    },
+    {
+      name: "mahtab",
+      email: "withgaming50@gmail.com",
+    },
+  ];
+  res.status(200).send(data);
+});
